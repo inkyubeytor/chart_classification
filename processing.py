fef4f05a3939a2891a64c0b0ce32be6bacc41818
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2020-07-16 18:53:35 -0400

    Added numpy array processing functions

--- processing.py
+++ processing.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from typing import Optional
+import numpy as np
 from PIL import Image
 
 # Scaling dimensions
@@ -80,3 +81,37 @@ def scale_image(fp: str, dest: Optional[str] = None) -> str:
     new_path = dest or fp
     img.save(new_path)
     return new_path
+
+
+def make_array(fp: str) -> np.ndarray:
+    """
+    Converts an image on disk to a numpy array.
+    :param fp: The image to convert.
+    :return: The image data as a numpy array.
+    """
+    img = Image.open(fp)
+    arr = np.array(img)
+    img.close()
+    return arr
+
+
+def scale_pixels(arr: np.ndarray) -> np.ndarray:
+    """
+    Scales the pixel values of an image array to the range [0, 1]. Always
+    returns an out-of-place copy.
+    :param arr: An array of pixel values representing an image.
+    :return: An array representing the same image with pixel values in [0, 1].
+    """
+    if arr.max(initial=0.0) > 1.0:
+        return arr / 255.0
+    else:
+        return arr / 1.0
+
+
+def flatten(arr: np.ndarray) -> np.ndarray:
+    """
+    Flattens the given image array.
+    :param arr: An array representing an image.
+    :return: A copy of the array collapsed into one dimension.
+    """
+    return arr.flatten()
