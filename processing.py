e073c5f8b9c970f744114b955433bde85f4c751b
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2020-07-16 14:20:38 -0400

    Added function to map over processes for faster operations.

--- processing.py
+++ processing.py
@@ -1,11 +1,15 @@
 import os
 import shutil
-from typing import Optional
+from typing import Callable, List, Optional
+import multiprocessing
 from PIL import Image
 
+# Process count for multiprocessing
+POOL_SIZE = 4
 # Scaling dimensions
 HEIGHT = 300
 WIDTH = 400
+
 
 def _create_tmp() -> None:
     """
@@ -79,3 +83,18 @@ def scale_image(fp: str, dest: Optional[str] = None) -> str:
     new_path = dest or fp
     img.save(new_path)
     return new_path
+
+
+def process_map(f: Callable, args: List, packed: bool = False) -> List:
+    """
+    Maps an operation from processing.py across multiple processes.
+    :param f: The function to map, from processing.py.
+    :param args: The list of argument tuples to map over.
+    :param packed: Whether the args list consists of packed argument tuples.
+    :return: The list of outputs from the mapping of f over args.
+    """
+    with multiprocessing.Pool(POOL_SIZE) as p:
+        if packed:
+            return p.starmap(f, args)
+        else:
+            return p.map(f, args)
