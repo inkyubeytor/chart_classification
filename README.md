2b8f3a06d6432061cf70148d40db0b48cd5bf1fa
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2020-07-17 17:26:46 -0400

    Partially implemented image downloading

--- README.md
+++ README.md
@@ -4,14 +4,6 @@
 TODO:
 * Get an image from a source
   * Via URL
-  * From on disk
-
-* Convert a raw image to an input
-  * Convert to png
-  * Scale to 300H x 400W
-  * Convert to grayscale
-  * Convert to numpy array
-  * Scale to be in the range [0, 1]
 
 * Train/Classify
   * Load training data
--- processing/retrieval.py
+++ processing/retrieval.py
@@ -1,13 +1,65 @@
 import os
 import shutil
+import requests
+from typing import Optional
+import time
+
+IMAGE_FORMATS = ["jpg", "jpeg", "png", "gif", "tiff", "tif", "bmp"]
 
 
-def copy_to_store(fp: str) -> str:
+def _get_filetype_from_name(name: str) -> Optional[str]:
+    """
+    Checks whether a file name is an image file name.
+    :param name: The filename to check (not a path).
+    :return: The file extension if it is a valid image extension..
+    """
+    extension = name.split('.')[-1]
+    if extension in IMAGE_FORMATS:
+        return extension
+    else:
+        return None
+
+
+def _check_filetype(fp: str, extension: str) -> bool:
+    """
+    Checks whether a file is an image file encoding.
+    :param fp: The file to check.
+    :return: True if the file is an image
+    """
+    raise NotImplementedError
+
+
+def copy_to_store(fp: str) -> Optional[str]:
     """
     Copies an image to data folder.
     :param fp: The image to copy.
     :return: A path to the new image.
     """
-    new_path = f"data/images/{os.path.basename(fp)}"
-    shutil.copyfile(fp, new_path)
-    return new_path
+    name = os.path.basename(fp)
+    extension = _get_filetype_from_name(name)
+    if extension and _check_filetype(fp, extension):
+        new_path = f"data/images/{time.time()}.{extension}"
+        shutil.copyfile(fp, new_path)
+        return new_path
+    else:
+        return None
+
+
+def download_to_store(url: str) -> Optional[str]:
+    """
+    Downloads an image from a URL to data folder.
+    :param url: The URL of the image to download.
+    :return: A path to the new image.
+    """
+    name = url.split('/')[-1].lower()
+    extension = _get_filetype_from_name(name)
+    if extension:
+        response = requests.get(url)
+        new_path = f"data/images/{time.time()}.{extension}"
+        with open(new_path, "wb") as f:
+            f.write(response.content)
+        if _check_filetype(new_path, extension):
+            return new_path
+        else:
+            os.remove(new_path)
+            return None
